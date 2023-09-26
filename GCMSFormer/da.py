f35bb4d6b74881ab20a6f113cd05ada73ea7646f
zxgcsu <22311020@csu.edu.cn>
2023-09-26 09:45:27 +0800

    add GCMFormer file

---
+++ GCMSFormer/da.py
@@ -0,0 +1,246 @@
+import numpy as np
+import random
+from tqdm import tqdm
+from scipy import stats
+import torch
+
+def data_pre(mss, max_mz, maxn, noise):
+    "A sample data: R"
+    global x0, means, sigmas, nums, ratios, a, b
+    if maxn == 5:
+        L0 = [random.uniform(0.115, 0.125),
+              random.uniform(0.125, 0.15),
+              random.uniform(0.15, 0.175),
+              random.uniform(0.175, 0.2)]
+        L = [random.uniform(0.115, 0.125)]
+        for i in range(3):
+            L.append(random.choice(L0))
+        random.shuffle(L)
+        m1 = random.randint(100, 300) * 0.001
+        m2 = m1 + L[0]
+        m3 = m2 + L[1]
+        m4 = m3 + L[2]
+        m5 = m4 + L[3]
+        means1 = [m1, m2, m3, m4, m5]
+        sigmas = random.randint(8, 14) * 0.01
+        ratios = [random.uniform(0.3, 2), random.uniform(0.5, 2), random.uniform(0.5, 2), random.uniform(0.5, 2),
+                  random.uniform(0.3, 2)]
+        nums = random.randint(40, 100)
+        if sigmas <= 0.1:
+            means = means1
+            a, b = -0.4, 1.5
+            x0 = np.linspace(a, b, nums)
+        else:
+            means = means1
+            a, b = -0.4, 1.5
+            x0 = np.linspace(a, b, nums)
+    if maxn == 4:
+        L0 = [random.uniform(0.115, 0.145),
+              random.uniform(0.145, 0.175),
+              random.uniform(0.175, 0.2)]
+        L = [random.uniform(0.115, 0.125)]
+        for i in range(2):
+            L.append(random.choice(L0))
+        random.shuffle(L)
+        m1 = random.randint(200, 280) * 0.001
+        m2 = m1 + L[0]
+        m3 = m2 + L[1]
+        m4 = m3 + L[2]
+        means1 = [m1, m2, m3, m4]
+        sigmas = random.randint(8, 14) * 0.01
+        ratios = [random.uniform(0.3, 2), random.uniform(0.5, 2), random.uniform(0.5, 2), random.uniform(0.3, 2)]
+        nums = random.randint(30, 90)
+        if sigmas <= 0.1:
+            means = means1
+            a, b = -0.2, 1.5
+            x0 = np.linspace(a, b, nums)
+        else:
+            means = means1
+            a, b = -0.3, 1.5
+            x0 = np.linspace(a, b, nums)
+    if maxn == 3:
+        L0 = [random.uniform(0.115, 0.125),
+              random.uniform(0.125, 0.15)]
+        L = [random.uniform(0.115, 0.125)]
+        for i in range(1):
+            L.append(random.choice(L0))
+        random.shuffle(L)
+        m1 = random.randint(200, 280) * 0.001
+        m2 = m1 + L[0]
+        m3 = m2 + L[1]
+        means1 = [m1, m2, m3]
+        sigmas = random.randint(8, 14) * 0.01
+        ratios = [random.uniform(0.5, 2), random.uniform(0.5, 2), random.uniform(0.5, 2)]
+        nums = random.randint(20, 80)
+        if sigmas <= 0.1:
+            means = means1
+            a, b = -0.2, 1.4
+            x0 = np.linspace(a, b, nums)
+        else:
+            means = means1
+            a, b = -0.5, 1.6
+            x0 = np.linspace(a, b, nums)
+    if maxn == 2:
+        L = [random.uniform(0.115, 0.125)]
+        m1 = random.randint(200, 280) * 0.001
+        m2 = m1 + L[0]
+        means1 = [m1, m2]
+        sigmas = random.randint(8, 14) * 0.01
+        maxn = 2
+        ratios = [random.uniform(0.3, 2), random.uniform(0.3, 2)]
+        nums = random.randint(20, 70)
+        if sigmas <= 0.1:
+            means = means1
+            a, b = -0.2, 1.2
+            x0 = np.linspace(a, b, nums)
+        else:
+            means = means1
+            a, b = -0.3, 1.55
+            x0 = np.linspace(a, b, nums)
+    if maxn == 1:
+        m1 = random.randint(200, 280) * 0.001
+        means1 = [m1]
+        sigmas = random.randint(8, 14) * 0.01
+        maxn = 1
+        ratios = [random.uniform(0.3, 2)]
+        nums = random.randint(20, 70)
+        if sigmas <= 0.1:
+            means = means1
+            a, b = -0.2, 1.2
+            x0 = np.linspace(a, b, nums)
+        else:
+            means = means1
+            a, b = -0.3, 1.55
+            x0 = np.linspace(a, b, nums)
+
+    ids4p = random.sample(range(0, len(mss)), maxn)
+
+    S = np.zeros((max_mz, maxn), dtype=np.float32)
+    C = np.zeros((nums, maxn), dtype=np.float32)
+    for i in range(maxn):
+        C[:, i] = stats.norm.pdf(x0, means[i], sigmas) * ratios[i]
+        S[:, i] = mss[ids4p[i]] / np.sum(mss[ids4p[i]])
+    X_0 = np.dot(C, S.T)
+
+    para = np.random.randn(nums, max_mz)
+    E0 = para - np.min(para)
+    E = E0 * (np.max(np.sum(X_0, 1)) * noise / (E0.sum() / nums))
+    X = np.float32((np.dot(C, S.T) + E) / np.max(np.dot(C, S.T) + E))
+    total = ratios + means + [sigmas, a, b, nums, maxn]
+    return X, X_0, S, ids4p, total
+
+def read_msp_MZmine(msp_file_path, d_models):
+    f = open(msp_file_path).read()
+    a = f.split('\n')
+    m = []
+    n = []
+    for i, l in enumerate(a):
+        if l.startswith('Name'):
+            m.append(i)
+        if l.startswith('Num Peaks'):
+            n.append(i)
+    mss = np.zeros((0, 1000), dtype=np.float32)
+    for t in range(len(m) - 1):
+        mzs = []
+        ins = []
+        for j in range(n[t] + 1, m[t + 1] - 1):
+            ps = a[j].split('\n ')
+            ps = [p for p in ps if len(p) > 0]
+            for p in ps:
+                mz_in = p.split(' ')
+                mzs.append(int(float(mz_in[0])))
+                ins.append(np.float32((mz_in[1])))
+
+        ms = np.zeros((1, 1000), dtype=np.float32)
+        for i, mz in enumerate(mzs):
+            ms[0, mz - 1] = ins[i]
+        mss = np.vstack((mss, ms / np.max(ms)))
+
+    mzs = []
+    ins = []
+    for j in range(n[-1] + 1, len(a)):
+        ps = a[j].split('\n ')
+        ps = [p for p in ps if len(p) > 0]
+        for p in ps:
+            mz_in = p.split(' ')
+            mzs.append(int(float(mz_in[0])))
+            ins.append(float(mz_in[1]))
+    ms = np.zeros((1, 1000), dtype=np.float32)
+    for i, mz in enumerate(mzs):
+        ms[0, mz - 1] = ins[i]
+    mss = np.vstack((mss, ms / np.max(ms)))
+
+    RT = []
+    for t in range(len(m)):
+        ps = a[m[t] + 2].split('\n ')
+        ps = [p for p in ps if len(p) > 0]
+        for p in ps:
+            mz_rt = p.split(' ')
+            RT.append(np.float32((mz_rt[1])))
+    return RT, mss
+
+def data_augmentation(msp_file_path, d_models, n, noise_level=0.001):
+
+    "Obtain datasets by data augmentation."
+    RT, mss = read_msp_MZmine(msp_file_path, d_models)
+    DATA = []
+    TARGET = []
+    TOTAL = []
+    for i in tqdm(range(n), desc='Generating Dataset'):
+        noise = random.randint(1, 50) * noise_level
+        maxn = random.randint(1, 5)
+        data, data_0, target, ids4p, totals = data_pre(mss, d_models, maxn, noise)
+        DATA.append(torch.tensor(data))
+        TARGET.append(torch.tensor(target.T))
+        TOTAL.append(torch.tensor(totals))
+    ms = np.zeros_like(mss)
+    for i in range(len(mss)):
+        ms[i] = mss[i] / np.sum(mss[i])
+    tgt_vacob = torch.tensor(ms, dtype=torch.float)
+    bos = torch.cat((torch.ones([1, int(d_models / 2)], dtype=torch.float),
+                     torch.zeros([1, int(d_models / 2)], dtype=torch.float)), dim=1)
+    eos = torch.cat((torch.zeros([1, int(d_models / 2)], dtype=torch.float),
+                     torch.ones([1, int(d_models / 2)], dtype=torch.float)), dim=1)
+    pad = torch.zeros([1, d_models], dtype=torch.float)
+    tgt_vacob = torch.cat((pad, bos, eos, tgt_vacob), dim=0)
+    TARGET_ind = []
+    for t in TARGET:
+        ind = []
+        for i in range(len(t)):
+            for j in range(len(tgt_vacob)):
+                if t[i].equal(tgt_vacob[j]):
+                    ind.append(j)
+        TARGET_ind.append(torch.tensor(ind))
+
+    return DATA, TARGET, tgt_vacob, TARGET_ind, TOTAL
+
+def data_split(aug_num, d_models, msp_file_path, validation_split):
+
+    DATA, TARGET, tgt_vacob, TARGET_ind, TOTAL = data_augmentation(msp_file_path, d_models, aug_num, noise_level=0.001)
+    train_src = DATA[0:round((1 - validation_split) * aug_num)]
+    train_tgt = TARGET[0:round((1 - validation_split) * aug_num)]
+    train_tgt_ind = TARGET_ind[0:round((1 - validation_split) * aug_num)]
+    train_total = TOTAL[0:round((1 - validation_split) * aug_num)]
+    TRAIN = tuple((train_src, train_tgt, train_tgt_ind, train_total))
+
+    valid_src = DATA[round((1 - validation_split) * aug_num):round(0.9 * aug_num)]
+    valid_tgt = TARGET[round((1 - validation_split) * aug_num):round(0.9 * aug_num)]
+    valid_tgt_ind = TARGET_ind[round((1 - validation_split) * aug_num):round(0.9 * aug_num)]
+    valid_total = TOTAL[round((1 - validation_split) * aug_num):round(0.9 * aug_num)]
+    VALID = tuple((valid_src, valid_tgt, valid_tgt_ind, valid_total))
+
+    test_src = DATA[round(0.9 * aug_num):aug_num]
+    test_tgt = TARGET[round(0.9 * aug_num):aug_num]
+    test_tgt_ind = TARGET_ind[round(0.9 * aug_num):aug_num]
+    test_total = TOTAL[round(0.9 * aug_num):aug_num]
+    TEST = tuple((test_src, test_tgt, test_tgt_ind, test_total))
+
+    return TRAIN, VALID, TEST, tgt_vacob
+
+def gen_datasets(para):
+    aug_nums = para['aug_num']
+    validation_split = 0.2
+    msp_file_path = para['name']
+    d_models = int(max(para['mz_range']))
+    TRAIN, VALID, TEST, tgt_vacob = data_split(aug_nums, d_models, msp_file_path, validation_split)
+    return TRAIN, VALID, TEST, tgt_vacob
--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 ## GCMSFormer
 
-This is the code repo for GCMSFormer mehtod. We proposed the GCMSFormer for resolving the overlapped peaks in complex GC-MS data based on a Transformer model. The GCMSFormer model was trained, validated, and tested with 30,000 augmented simulated overlapped peaks in a ratio of 8:1:1, and its bilingual evaluation understudy (BLEU) on the test set was 0.98774. With the aid of the orthogonal projection resolution method (OPR), GCMSFormer can predict the pure mass spectra of all components in overlapped peaks (mass spectral matrix S), and then use the least squares method to find the concentration distribution matrix C. The automatic resolution of the overlapped peaks can be easily achieved.
+This is the code repo for GCMSFormer mehtod. We proposed the GCMSFormer for resolving the overlapped peaks in complex GC-MS data based on a Transformer model. The GCMSFormer model was trained, validated, and tested with 100,000 augmented simulated overlapped peaks in a ratio of 8:1:1, and its bilingual evaluation understudy (BLEU) on the test set was 0.99722. With the aid of the orthogonal projection resolution method (OPR), GCMSFormer can predict the pure mass spectra of all components in overlapped peaks (mass spectral matrix S), and then use the least squares method to find the concentration distribution matrix C. The automatic resolution of the overlapped peaks can be easily achieved.
 
-![](https://github.com/Xuaner111/GCMSFormer/blob/master/workflow.png)
+![](https://github.com/zxguocsu/GCMSFormer/blob/master/workflow.png)
 
 ### Package required:
 We recommend to use [conda](https://conda.io/docs/user-guide/install/download.html) and [pip](https://pypi.org/project/pip/).
 - [python3](https://www.python.org/)
 - [pytorch](https://pytorch.org/) 
 
-By using the [`environment.yml`](https://github.com/Xuaner111/GCMSFormer/blob/master/environment.yml), [`requirements.txt`](https://github.com/Xuaner111/GCMSFormer/blob/master/requirements.txt) file, it will install all the required packages.
+By using the [`environment.yml`](https://github.com/zxguocsu/GCMSFormer/blob/master/environment.yml), [`requirements.txt`](https://github.com/zxguocsu/GCMSFormer/blob/master/requirements.txt) file, it will install all the required packages.
 
-    git clone https://github.com/Xuaner111/GCMSFormer.git
+    git clone https://github.com/zxguocsu/GCMSFormer.git
     cd GCMSFormer
     conda env create -f environment.yml
     conda activate GCMSFormer
     
 ## Data augmentation
 
-The overlapped peak dataset for training, validating and testing the GCMSFormer model is obtained using the [gen_datasets](https://github.com/Xuaner111/GCMSFormer/blob/master/GCMSFormer/da.py#L248) functions.
+The overlapped peak dataset for training, validating and testing the GCMSFormer model is obtained using the [gen_datasets](https://github.com/zxguocsu/GCMSFormer/blob/master/GCMSFormer/da.py#L240) functions.
 
     TRAIN, VALID, TEST, tgt_vacob = gen_datasets(para)
 
@@ -26,7 +26,7 @@ The overlapped peak dataset for training, validating and testing the GCMSFormer 
 - para : Data augmentation parameters 
 
 ## Model training
-Train the model based on your own training dataset with [train_model](https://github.com/Xuaner111/GCMSFormer/blob/master/GCMSFormer/GCMSformer.py#L428) function.
+Train the model based on your own training dataset with [train_model](https://github.com/zxguocsu/GCMSFormer/blob/master/GCMSFormer/GCMSformer.py#L408) function.
 
     model, Loss = train_model(para, TRAIN, VALID, tgt_vacob)
 
@@ -38,7 +38,7 @@ Train the model based on your own training dataset with [train_model](https://gi
 
 ## Resolution
 
-Automatic Resolution of GC-MS data files by using the [Resolution](https://github.com/Xuaner111/GCMSFormer/blob/master/GCMSFormer/Resolution.py#L49) function.
+Automatic Resolution of GC-MS data files by using the [Resolution](https://github.com/zxguocsu/GCMSFormer/blob/master/GCMSFormer/Resolution.py#L51) function.
 
     Resolution(path, filename, model, tgt_vacob, device)
     
@@ -50,10 +50,11 @@ Automatic Resolution of GC-MS data files by using the [Resolution](https://githu
 - device : Data distribution devices (cuda/cpu)
 
 ## Clone the repository and run it directly
-[git clone](https://github.com/Xuaner111/GCMSFormer)
+[git clone](https://github.com/zxguocsu/GCMSFormer)
 
-An example has been provided in [test_new.ipynb](https://github.com/Xuaner111/GCMSFormer/blob/master/test_new.ipynb) 
-script for the convenience of users. The GC-MS file used in it is available in the file [data](https://github.com/Xuaner111/GCMSFormer/tree/master/data).
+An example has been provided in [test.ipynb](https://github.com/zxguocsu/GCMSFormer/blob/master/test_new.ipynb) 
+script for the convenience of users. The GC-MS file used in it is available in the file [Essential Oil Data](https://github.com/zxguocsu/GCMSFormer/releases/tag/v1.0.0).
 
 ## Contact
-- 549648385@qq.com
+- zmzhang@csu.edu.cn
+- 222311020@csu.edu.cn
